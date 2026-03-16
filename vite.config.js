022e305ccf96043593dbe070e7143913fbae2428
Nishant Jain <nishantjain@ArcherL-MCB.local>
2026-03-16 00:17:44 -0700

    deploy deploy deploy

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: './',
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         drawTrack: resolve(__dirname, 'draw-track.html'),
+        drawLabels: resolve(__dirname, 'draw-labels.html'),
       },
     },
   },
