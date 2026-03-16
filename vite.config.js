4fd08ef057c81bcbc98d94417f2d9a27a5914a4e
Nishant Jain <nishantjain@ArcherL-MCB.local>
2026-03-15 22:01:49 -0700

    vite build

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        drawTrack: resolve(__dirname, 'draw-track.html')
+      }
+    }
+  }
+})
