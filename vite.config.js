3e5795d7c071c00518de3840002959e40786f48f
Nishant Jain <nishantjain@ArcherL-MCB.local>
2026-06-27 18:51:42 -0700

    tri-athalon

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         f1_miami: resolve(__dirname, 'f1-miami.html'),
+        triathlon: resolve(__dirname, 'triathlon.html'),
         // drawTrack: resolve(__dirname, 'draw-track.html'),
         // drawLabels: resolve(__dirname, 'draw-labels.html'),
       },
